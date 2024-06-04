597578aebc273cbac86c4a9344577e3a73edfe27
castermode <castermode@gmail.com>
2024-06-04 18:37:25 +0800

    add code

--- src/test/tcp_client_test.cc
+++ src/test/tcp_client_test.cc
@@ -66,42 +66,40 @@ TEST(TcpClient, TcpClient) {
   std::cout << "loop stop" << std::endl;
 }
 
-#if 0
 TEST(TcpClient, TcpClient2) {
   system("rm -f /tmp/tcpclient_test.log");
   vraft::LoggerOptions o;
   o.logger_name = "TcpClient.TcpClient2";
   vraft::vraft_logger.Init("/tmp/tcpclient_test.log", o);
 
-  vraft::EventLoop loop("test_loop");
-  vraft::EventLoop *l = &loop;
+  vraft::EventLoopSPtr loop = std::make_shared<vraft::EventLoop>("test-loop");
+  int32_t rv = loop->Init();
+  ASSERT_EQ(rv, 0);
 
   vraft::HostPort dest_addr("baidu.com:80");
   vraft::TcpOptions to;
-  vraft::TcpClient tcp_client("tcp_client", l, dest_addr, to);
+  vraft::TcpClient tcp_client(loop, "tcp_client", dest_addr, to);
   vraft::TcpClient *pt = &tcp_client;
 
-  std::thread t([l]() { l->Loop(); });
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::thread t([loop]() { loop->Loop(); });
+  loop->WaitStarted();
 
-  l->RunFunctor([pt]() {
+  loop->RunFunctor([pt]() {
     pt->Connect();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << pt->ToString() << std::endl;
+    std::cout << pt->DebugString() << std::endl;
   });
 
   std::this_thread::sleep_for(std::chrono::seconds(2));
-  l->RunFunctor([pt]() { pt->Stop(); });
-  std::thread t2([l]() {
+  loop->RunFunctor([pt]() { pt->Stop(); });
+  std::thread t2([loop]() {
     std::cout << "after 3s, call loop stop() ..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(3));
-    l->Stop();
+    loop->Stop();
   });
   t.join();
   t2.join();
   std::cout << "loop stop" << std::endl;
 }
-#endif
 
 int main(int argc, char **argv) {
   vraft::CodingInit();
